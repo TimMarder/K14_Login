193764c934b5b2d6daa2eccb21c096bf2574d910
TimMarder <tmarder@stuy.edu>
2018-10-02 00:30:53 -0400

    Cleaned up code. Robustified.

--- app.py
+++ app.py
@@ -13,55 +13,38 @@ import os
 
 
 app = Flask(__name__)
+app.secret_key = os.urandom(32)
 
 @app.route("/")
-def hello_world():
-    if (session.get('username') == "peglegs"):
-        return redirect(url_for("login_success"))
+def home():
+    if (session.get('username') == "timian"):
+        return render_template("welcome.html",
+                                user = "timian")
     else:
-        print(app)
-        print(request)
-        print(request.args)
-        print(request.headers)
-        return render_template("home.html",
-                                username = request.cookies.get('username'),
-                                password = request.cookies.get('password'),
-                                user = "peglegs",
-                                pwd = "oxana")
+        return render_template("home.html")
 
 
-
-@app.route("/loginsuccess")
-def login_success():
-    session['username'] = "peglegs"
-    return render_template("welcome.html",
-                            username = request.cookies.get('username'),
-                            password = request.cookies.get('password'),
-                            user = "peglegs",
-                            pwd = "oxana")
 
 @app.route("/logout")
 def log_out():
     session.pop('username')
-    return redirect(url_for("hello_world"))
+    return redirect(url_for("home"))
 
-@app.route("/loginfail")
-def login_fail():
-    return render_template("error.html")
 
-@app.route("/login")
-def login():
-    return render_template("login.html")
 
 @app.route("/auth", methods = ["GET", "POST"])
 def authenticate():
-    if ((request.form['username'] == "peglegs") & (request.form['password'] == "oxana")):
-        return redirect(url_for("login_success"))
+    if ((request.form['username'] == "timian") &
+        (request.form['password'] == "wasilarder")):
+        session['username'] = "timian"
+        return render_template("welcome.html",
+                                user = "timian")
     else:
-        return redirect(url_for("login_fail"))
+        return render_template("error.html",
+                                user = request.form['username'],
+                                pwd = request.form['password'])
     #return render_template("auth.html")
 
 if __name__ == "__main__":
     app.debug = True
-    app.config['SECRET_KEY'] = os.urandom(32)
     app.run()
