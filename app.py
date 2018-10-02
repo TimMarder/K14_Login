979892ae5561a167a336207930f644f9341a7bba
Damian Wasilewicz <dwasilewicz@stuy.edu>
2018-10-02 01:00:18 -0400

    added cookies, comments, heading, improved aesthetic, added specification for which part of username password combo failed

--- app.py
+++ app.py
@@ -1,8 +1,9 @@
-# Tim Marder
+# Polish-Israeli Coding Confederacy
 # SoftDev1 pd06
-# K#13 -- Echo Echo Echo
-# 2018-09-28
+# K #14: Do I Know You?
+# 2018-10-01
 
+#imports necessary tools from flask and os
 from flask import Flask
 from flask import render_template
 from flask import request
@@ -15,23 +16,29 @@ import os
 app = Flask(__name__)
 app.secret_key = os.urandom(32)
 
+
+#determines whether or not user is currently logged in by checking if the session
+# "dictionary" has a key with appropriate username; displays welcome page if logged in,
+#sends user to home to sign in if user isn't logged in
 @app.route("/")
 def home():
-    if (session.get('username') == "timian"):
+    if request.cookies.get('username'):
         return render_template("welcome.html",
                                 user = "timian")
     else:
         return render_template("home.html")
 
 
-
+#removes user from session, logging them out
 @app.route("/logout")
 def log_out():
     session.pop('username')
     return redirect(url_for("home"))
 
 
-
+#home page; determines whether input information is correct
+#and returns welcome page if login successful, home page if not
+#error page includes hint as to which part of login combo was wrong
 @app.route("/auth", methods = ["GET", "POST"])
 def authenticate():
     if ((request.form['username'] == "timian") &
@@ -39,11 +46,21 @@ def authenticate():
         session['username'] = "timian"
         return render_template("welcome.html",
                                 user = "timian")
+    elif ((request.form['username']) == "timian"):
+        return render_template("error.html",
+                                user = request.form['username'],
+                                pwd = request.form['password'],
+                                error = "Your password was incorrect")
+    elif ((request.form['password']) == "wasilarder"):
+        return render_template("error.html",
+                                user = request.form['username'],
+                                pwd = request.form['password'],
+                                error = "Your username was incorrect")
     else:
         return render_template("error.html",
                                 user = request.form['username'],
-                                pwd = request.form['password'])
-    #return render_template("auth.html")
+                                pwd = request.form['password'],
+                                error = "Your password and password were both incorrect!")
 
 if __name__ == "__main__":
     app.debug = True
